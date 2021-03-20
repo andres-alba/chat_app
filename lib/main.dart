7168541ca9c19683d0a814fb4cb0d5e1c96949ec
Andres <andresalba34@gmail.com>
2021-03-20 15:46:37 -0500

    - Completed login and Registration Screens - Worked on Stream Builder in Chat Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'package:chat_app/screens/welcome_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/screens/registration_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: Colors.black54),
-        ),
-      ),
       home: WelcomeScreen(),
       initialRoute: WelcomeScreen.id,
       routes: {
